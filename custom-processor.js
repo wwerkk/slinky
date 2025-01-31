5ee499e884430b359f79dcac1b87ff2c36202541
wwerkk <wwerkowicz@gmail.com>
2025-01-31 01:29:22 +0100

    custom-processor.js refactor

--- custom-processor.js
+++ custom-processor.js
@@ -1,4 +1,3 @@
-// custom-processor.js
 class CustomPlaybackProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
@@ -7,25 +6,38 @@ class CustomPlaybackProcessor extends AudioWorkletProcessor {
         this.playbackRate = 1; // Playback rate
         this.loop = false; // Looping state
         this.isPlaying = false; // Playback state
-        this.port.onmessage = (event) => {
-            const { action, buffer, rate, loop } = event.data;
-            console.log(event.data);
-            if (action === 'load') {
-                this.buffer = new Float32Array(buffer); // Copy the buffer
-                console.log('Buffer loaded:', this.buffer);
-            } else if (action === 'play') {
-                this.currentFrame = 0; // Reset playback position
-                this.isPlaying = true;
-            } else if (action === 'stop') {
-                this.isPlaying = false;
-            } else if (action === 'setRate') {
-                this.playbackRate = rate;
-            } else if (action === 'loop') {
-                this.loop = loop;
-            }
-        };
+
+        // Handle messages from the main thread
+        this.port.onmessage = (event) => this.handleMessage(event);
     }
 
+    // Handle incoming messages
+    handleMessage(event) {
+        const { action, buffer, rate, loop } = event.data;
+        console.log('Message received:', event.data);
+
+        switch (action) {
+            case 'load':
+                this.buffer = new Float32Array(buffer); // Copy the buffer
+                console.log('Buffer loaded:', this.buffer);
+                break;
+            case 'play':
+                this.currentFrame = 0; // Reset playback position
+                this.isPlaying = true;
+                break;
+            case 'stop':
+                this.isPlaying = false;
+                break;
+            case 'setRate':
+                this.playbackRate = rate;
+                break;
+            case 'loop':
+                this.loop = loop;
+                break;
+        }
+    }
+
+    // Audio processing loop
     process(inputs, outputs, parameters) {
         if (!this.isPlaying || !this.buffer) return true;
 
@@ -33,19 +45,23 @@ class CustomPlaybackProcessor extends AudioWorkletProcessor {
         const channelCount = output.length;
 
         for (let i = 0; i < output[0].length; i++) {
-            if (this.loop && this.currentFrame >= this.buffer.length)
+            if (this.loop && this.currentFrame >= this.buffer.length) {
                 this.currentFrame = this.currentFrame % this.buffer.length;
+            }
+
             if (this.currentFrame < this.buffer.length) {
                 const frameIndex = Math.floor(this.currentFrame);
                 const fraction = this.currentFrame - frameIndex;
                 const interpolatedSample = this.buffer[frameIndex] +
                     fraction * (this.buffer[frameIndex + 1] - this.buffer[frameIndex]);
+
                 for (let channel = 0; channel < channelCount; channel++) {
                     output[channel][i] = interpolatedSample;
                 }
+
                 this.currentFrame += this.playbackRate;
             } else {
-                this.isPlaying = false;
+                this.isPlaying = false; // Stop playback when the buffer ends
                 break;
             }
         }
