7673a4eae6a4c9898dc27d32b42e51f24721f473
wwerkk <wwerkowicz@gmail.com>
2025-08-01 01:36:54 +0200

    upscale waveform plot

--- src/waveform.js
+++ src/waveform.js
@@ -4,6 +4,7 @@ export class Waveform {
         this.ctx = this.canvas.getContext('2d');
         this.playhead = document.getElementById(playheadId);
         this.currentBuffer = null;
+        this.upscaleFactor = 4;
         this.updateCanvasSize();
 
         window.addEventListener('resize', () => this.handleResize());
@@ -40,19 +41,54 @@ export class Waveform {
         this.ctx.lineWidth = 1;
         this.ctx.lineCap = 'round';
 
-        this.ctx.beginPath();
-
-        for (let i = 0; i < this.canvasWidth; i++) {
-            const sampleIndex = Math.floor((i * dataLength) / this.canvasWidth);
-            const sample = channelData[sampleIndex] || 0;
-
-            const x = i;
-            const y = amp + (sample * amp);
-
-            if (i === 0) {
-                this.ctx.moveTo(x, y);
+        const highResWidth = this.canvasWidth * this.upscaleFactor;
+        
+        const waveformPoints = [];
+        for (let i = 0; i < highResWidth; i++) {
+            const samplePosition = (i * dataLength) / highResWidth;
+            
+            let sample;
+            if (samplePosition >= dataLength - 1) {
+                sample = channelData[dataLength - 1] || 0;
+            } else if (samplePosition <= 0) {
+                sample = channelData[0] || 0;
             } else {
-                this.ctx.lineTo(x, y);
+                const lowerIndex = Math.floor(samplePosition);
+                const upperIndex = Math.ceil(samplePosition);
+                const fraction = samplePosition - lowerIndex;
+                const lowerSample = channelData[lowerIndex] || 0;
+                const upperSample = channelData[upperIndex] || 0;
+                sample = lowerSample + (upperSample - lowerSample) * fraction;
+            }
+            
+            waveformPoints.push(sample);
+        }
+        
+        this.ctx.beginPath();
+        for (let i = 0; i < this.canvasWidth; i++) {
+            const startIdx = Math.floor((i * highResWidth) / this.canvasWidth);
+            const endIdx = Math.floor(((i + 1) * highResWidth) / this.canvasWidth);
+            
+            let min = 0, max = 0;
+            for (let j = startIdx; j < endIdx && j < waveformPoints.length; j++) {
+                const value = waveformPoints[j];
+                if (value < min) min = value;
+                if (value > max) max = value;
+            }
+            
+            const x = i;
+            const yMin = amp + (min * amp);
+            const yMax = amp + (max * amp);
+            
+            if (i === 0) {
+                this.ctx.moveTo(x, yMax);
+            } else {
+                this.ctx.lineTo(x, yMax);
+            }
+            
+            if (Math.abs(yMax - yMin) > 1) {
+                this.ctx.lineTo(x, yMin);
+                this.ctx.lineTo(x, yMax);
             }
         }
 
